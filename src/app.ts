4ff309854c84135ddec40038da5007ceb4ee255f
Novikov Dmitriy <dn@aube.work>
2025-06-05 10:07:06 +0300

    server address setup via .env

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import express from "express";
 import { apiRouter } from "./routes";
 import { ItemModel } from "./models";
 import * as middlewares from "./middlewares";
+import "dotenv/config";
 
 // init store
 new ItemModel();
+
+const PORT = Number(process.env.SERVER_PORT || 3000);
+const ADDRESS = String(process.env.SERVER_IP || "127.0.0.1");
 
 const app = express();
 
@@ -18,4 +22,6 @@ app.use(middlewares.midLogger);
 
 app.use(apiRouter());
 
-app.listen(3000);
+app.listen(PORT, ADDRESS, () => {
+  console.log("Server started at", ADDRESS, ", port:", PORT);
+});
